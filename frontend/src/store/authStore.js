dcc46efd70a2c519ba248330cd47358c7ac0d81e
MOHAMMAD AFTHAB <theaffu18@gmail.com>
2026-05-19 14:42:28 +0530

    updated

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,62 +1,119 @@
 import { create } from 'zustand'
 import axios from 'axios'
 import API_BASE from '../config/api'
+
 axios.defaults.withCredentials = true;
 
 export let useAuth = create((set) => ({
+
     currentUser: null,
     isAuthenticated: false,
     loading: true,
     error: null,
+
+    // LOGIN
     login: async (userCredObjWithRole) => {
-        const {role, ...userCredObj} = userCredObjWithRole
-        try{
-            set({ loading: true, error: null})
-            let res = await axios.post(`${API_BASE}/common-api/login`, userCredObj, {withCredentials: true})
+
+        const { role, ...userCredObj } = userCredObjWithRole
+
+        try {
+
+            set({
+                loading: true,
+                error: null
+            })
+
+            let res = await axios.post(
+                `${API_BASE}/common-api/login`,
+                userCredObj,
+                { withCredentials: true }
+            )
+
             set({
                 loading: false,
-                isAuthenticated: true,
+                isAuthenticated: !!res.data.payload,
                 currentUser: res.data.payload
             })
-        }catch(err){
+
+        } catch (err) {
+
             set({
                 loading: false,
                 error: err,
                 isAuthenticated: false,
                 currentUser: null
             })
+
         }
+
     },
+
+    // LOGOUT
     logout: async () => {
-        try{
-            set({ loading: true, error: null })
-            await axios.get(`${API_BASE}/common-api/logout`, { withCredentials: true})
-            set({ loading: false, currentUser: null, isAuthenticated: false})
-        }catch(err){
+
+        try {
+
+            set({
+                loading: true,
+                error: null
+            })
+
+            await axios.get(
+                `${API_BASE}/common-api/logout`,
+                { withCredentials: true }
+            )
+
+            set({
+                loading: false,
+                currentUser: null,
+                isAuthenticated: false
+            })
+
+        } catch (err) {
+
             set({
                 loading: false,
                 error: err,
                 isAuthenticated: false,
                 currentUser: null
             })
+
         }
+
     },
+
+    // CHECK AUTH
     checkAuth: async () => {
-        try{
-            set({loading: true, error: null})
-            const res = await axios.get(`${API_BASE}/common-api/check-auth?t=${Date.now()}`, {withCredentials: true})
+
+        try {
+
+            set({
+                loading: true,
+                error: null
+            })
+
+            const res = await axios.get(
+                `${API_BASE}/common-api/check-auth?t=${Date.now()}`,
+                { withCredentials: true }
+            )
+
             set({
                 loading: false,
                 currentUser: res.data.payload,
-                isAuthenticated: true
+                isAuthenticated: !!res.data.payload
             })
-        }catch(err){
+
+        } catch (err) {
+
             set({
                 loading: false,
                 error: err,
                 isAuthenticated: false,
                 currentUser: null
             })
+
         }
+
     }
+
 }))
