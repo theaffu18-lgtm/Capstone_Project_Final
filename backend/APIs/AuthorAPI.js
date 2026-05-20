d3318306d4468cd43e8287e2f48a47b87536abed
MOHAMMAD AFTHAB <theaffu18@gmail.com>
2026-05-20 10:07:10 +0530

    updated

--- backend/APIs/AuthorAPI.js
+++ backend/APIs/AuthorAPI.js
@@ -111,7 +111,7 @@ authorRoute.put("/articles",verifyToken("AUTHOR") ,checkAuthor,async (req, res) 
   let { articleId, title, category, content } = req.body;
 
 // get author from token (secure way)
-const userId = req.user._id;
+const userId = req.user.userId;
 console.log("User id",req.user._id)
 
 
@@ -151,7 +151,7 @@ authorRoute.patch("/articles/:id/status", verifyToken("AUTHOR"), async (req, res
   //console.log(req.user._id,article.author.toString())
   // AUTHOR can only modify their own articles
   if (req.user.role === "AUTHOR" && 
-    article.author.toString() !== req.user._id) {
+    article.author.toString() !== req.user.userId) {
     return res
     .status(403)
     .json({ message: "Forbidden. You can only modify your own articles" });
